916f9340fd84f555498cde086f994024a3be7695
lv-cha <zhaoxicn@foxmail.com>
2020-04-03 15:56:02 +0800

    chore: Add license file and update setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="top-zx",
+    version="0.0.1",
+    author="lvcha",
+    author_email="zhaoxicn@foxmail.com",
+    description="Taobao SDK for taobao spreader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lv-cha/tk_sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MulanPSL-1.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+)
